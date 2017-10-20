19f378e2a656656c036b1bb386b366ba26b0a4bd
yashchenko-oleksandr <sancho1996@gmail.com>
2017-10-20 20:52:44 +0300

    Начало 9 дз, задание 1

--- src/com/company/task9/Phone.java
+++ src/com/company/task9/Phone.java
@@ -2,23 +2,22 @@ package com.company.task9;
 
 /**
  * Задание 1
+ * Доделать класс Phone. (Задание с урока)
+ * з) Создать метод sendMessage  с аргументами переменной длины.
+ * Данный метод принимает на вход номера телефонов, которым будет отправлено сообщение. Метод выводит на консоль номера этих телефонов.  \
+ * и) Изменить класс Phone в соответствии с концепцией JavaBean.
+ * к) Реализовать подсчет количества созданных телефонов с помощью статической переменной.
  */
 public class Phone {
-    String number, model;
-    int weight;
-
-    void receiveCall(String name) {
-        System.out.println("Звонит " + name);
-    }
-
-    String getPhoneNumber() {
-        return number;
-    }
+    private String number, model;
+    private int weight;
+    static int count=0;
 
     Phone() {
         number = "+380932376456";
         model = "Lenova K5";
         weight = 432;
+        count++;
     }
 
     Phone(String number, String model, int weight) {
@@ -26,6 +25,38 @@ public class Phone {
         this.number = number;
         this.model = model;
         this.weight = weight;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    String getPhoneNumber() {
+        return number;
     }
 
     void receiveCall(String name, String numver) {
--- src/com/company/task9/PhoneDemo.java
+++ src/com/company/task9/PhoneDemo.java
@@ -6,33 +6,36 @@ public class PhoneDemo {
         Phone phone2 = new Phone();
         Phone phone3 = new Phone();
         phone1.receiveCall("Виталя");
-        phone1.number = "+380977434123";
-        System.out.println(phone1.getPhoneNumber());
-        phone1.model = "Apple 7";
-        phone1.weight = 123;
+        phone1.setNumber("+380977434123");
+        System.out.println(phone1.getNumber());
+        phone1.setModel("Apple 7");
+        phone1.setWeight(123);
 
         phone2.receiveCall("Саша");
-        phone2.number = "+380977434124";
-        System.out.println(phone2.getPhoneNumber());
-        phone2.model = "Samsung G8";
-        phone2.weight = 124;
+        phone2.setNumber("+380977434124");
+        System.out.println(phone2.getNumber());
+        phone2.setModel("Samsung G8");
+        phone2.setWeight(123);
 
         phone3.receiveCall("Бодя");
-        phone3.number = "+380977434125";
-        System.out.println(phone3.getPhoneNumber());
-        phone3.model = "HP";
-        phone3.weight = 125;
+        phone3.setNumber("+380977434125");
+        System.out.println(phone3.getNumber());
+        phone3.setModel("HP");
+        phone3.setWeight(125);
 
         Phone phone4 = new Phone();
-        System.out.println("Модель 4 обьекта = " + phone4.model);
+        System.out.println("Модель 4 обьекта = " + phone4.getModel());
 
         Phone phone5 = new Phone("+380994444444", "Nokia", 123);
-        System.out.println(phone5.number);
-        System.out.println(phone5.model);
-        System.out.println(phone5.weight);
+        System.out.println(phone5.getNumber());
+        System.out.println(phone5.getModel());
+        System.out.println(phone5.getWeight());
 
         phone5.receiveCall("Вася", "+380995555555 ");
 
-        phone5.sendMessage(phone1.getPhoneNumber(),phone2.getPhoneNumber());
+        phone5.sendMessage(phone1.getPhoneNumber(), phone2.getPhoneNumber());
+        phone5.sendMessage("2134324234", "32432423423");
+
+        System.out.println("Количество созданных телефонов = " + Phone.count);
     }
 }

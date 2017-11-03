826ec4586f6810b682322341c4d93b59cec41ac1
yashchenko-oleksandr <sancho1996@gmail.com>
2017-11-03 21:28:59 +0200

    Задание 13, пункт 1.

---
+++ src/com/company/task13/NumericString.java
@@ -0,0 +1,7 @@
+package com.company.task13;
+
+/**
+ * Created by Ukrainian IT_SCHOOL on 03.11.2017.
+ */
+public class NumericString {
+}
--- src/com/company/task13/Rows.java
+++ src/com/company/task13/Rows.java
@@ -5,13 +5,18 @@ package com.company.task13;
  */
 public class Rows {
     public static void main(String[] args) {
-        String row = "I like Java";
+        String row = "I like Java!!!";
         rowsMethod(row);
     }
 
-    public static void rowsMethod(String arg) {
-        System.out.println("Последний символ" + arg.charAt(arg.length() - 1));
-        System.out.println("Окончние строки сиволами '!!!'" + arg.endsWith("!!!"));
-        System.out.println("Начинается ли стока подстрокой 'I like'" + arg.startsWith("I like"));
+    public static void rowsMethod(String str) {
+        System.out.println("Последний символ - " + str.charAt(str.length() - 1));
+        System.out.println("Окончние строки сиволами '!!!' - " + str.endsWith("!!!"));
+        System.out.println("Начинается ли стока подстрокой 'I like' - " + str.startsWith("I like"));
+        System.out.println("Позиция сроки 'Java' - " + str.indexOf("Java"));
+        System.out.println("Заменить все символы а на о. Строка до замены: " + str +" После - "+ str.replace('a', 'o'));
+        System.out.println("Преобразование строки к верхнему регистру - " + str.toUpperCase());
+        System.out.println("Преобразование строки к нижнему регистру - " + str.toLowerCase());
+        System.out.println("Вырезать строку Java - "+str.substring(0,6)+str.substring(11,14));
     }
 }

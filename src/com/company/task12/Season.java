639143571da93765022a23a58fd339c05261dd02
yashchenko-oleksandr <sancho1996@gmail.com>
2017-11-05 17:33:36 +0200

    задание 12 пункт 1 выполнено!

--- src/com/company/task12/Season.java
+++ src/com/company/task12/Season.java
@@ -4,8 +4,24 @@ package com.company.task12;
  * Created by Ukrainian IT_SCHOOL on 30.10.2017.
  */
 public enum Season {
-    WINTER(-20), SPRING(14), SUMMER(30), AUTUMN(10);
+    WINTER(-20) {
+        public String getDescriptiom() {
+            return "Холодное время года!";
+        }
+    },
+    SPRING(14) {
+        public String getDescriptiom() {
+            return "Время года, когда все расцветает";
+        }
+    },
+    SUMMER(30),
+    AUTUMN(10) {
+        public String getDescriptiom() {
+            return "Время года когда все опадает=(";
+        }
+    };
     Season size;
+
     Season(int temperature) {
         this.temperature = temperature;
     }
@@ -15,4 +31,9 @@ public enum Season {
     public int getTemperature() {
         return temperature;
     }
+
+    public String getDescriptiom() {
+        return "Жаркое время года!";
+    }
+
 }

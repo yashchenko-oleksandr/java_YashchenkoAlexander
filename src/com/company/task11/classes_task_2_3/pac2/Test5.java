744aebbcd17c3ba30554cf06212d90455eba4c84
yashchenko-oleksandr <sancho1996@gmail.com>
2017-11-20 18:55:29 +0200

    Задание 11, Test5-отформатировано

--- src/com/company/task11/classes_task_2_3/pac2/Test5.java
+++ src/com/company/task11/classes_task_2_3/pac2/Test5.java
@@ -4,7 +4,7 @@ import com.company.task11.classes_task_2_3.pac1.Test1;
 
 public class Test5 {
     public void test() {
-        Test1 t1=new Test1();
-        t1.d=1;
+        Test1 t1 = new Test1();
+        t1.d = 1;
     }
 }

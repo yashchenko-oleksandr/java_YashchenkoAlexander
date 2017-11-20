909365e31d8ea4664230a054a7c6c1d7260dc435
yashchenko-oleksandr <sancho1996@gmail.com>
2017-11-20 18:59:38 +0200

    Задание 16, Калькулятор - отформатировано

--- src/com/company/task16/Calculator.java
+++ src/com/company/task16/Calculator.java
@@ -2,22 +2,25 @@ package com.company.task16;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.printf("Сумма чисел %s и %s = %.2f\n",1,2.343,sum(1,2.343));
-        System.out.printf("Умножение чисел %s и %s = %.2f\n",3,3.3f,multiply(3,3.3f));
-        System.out.printf("Деление чисел %s и %s = %.2f\n",454L,3.54,divide(454L,3.54));
-        System.out.printf("Разница чисел %s и %s = %.0f\n",124,546,subtraction(124,546));
+        System.out.printf("Сумма чисел %s и %s = %.2f\n", 1, 2.343, sum(1, 2.343));
+        System.out.printf("Умножение чисел %s и %s = %.2f\n", 3, 3.3f, multiply(3, 3.3f));
+        System.out.printf("Деление чисел %s и %s = %.2f\n", 454L, 3.54, divide(454L, 3.54));
+        System.out.printf("Разница чисел %s и %s = %.0f\n", 124, 546, subtraction(124, 546));
     }
 
-    static <T extends Number,V extends Number> double sum (T a, V b){
-        return a.doubleValue()+b.doubleValue();
+    static <T extends Number, V extends Number> double sum(T a, V b) {
+        return a.doubleValue() + b.doubleValue();
     }
-    static <T extends Number,V extends Number> double multiply(T a, V b){
-        return a.doubleValue()*b.doubleValue();
+
+    static <T extends Number, V extends Number> double multiply(T a, V b) {
+        return a.doubleValue() * b.doubleValue();
     }
-    static <T extends Number,V extends Number> double divide(T a, V b){
-        return a.doubleValue()/b.doubleValue();
+
+    static <T extends Number, V extends Number> double divide(T a, V b) {
+        return a.doubleValue() / b.doubleValue();
     }
-    static <T extends Number,V extends Number> double subtraction(T a, V b){
-        return a.doubleValue()-b.doubleValue();
+
+    static <T extends Number, V extends Number> double subtraction(T a, V b) {
+        return a.doubleValue() - b.doubleValue();
     }
 }

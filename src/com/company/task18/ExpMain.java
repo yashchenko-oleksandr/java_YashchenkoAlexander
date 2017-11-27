50f7aa3c33cf53bdc88cbfbe51355643c1f5d30d
yashchenko-oleksandr <sancho1996@gmail.com>
2017-11-27 21:01:54 +0200

    Task18, ExpMain

--- src/com/company/task18/ExpMain.java
+++ src/com/company/task18/ExpMain.java
@@ -5,26 +5,29 @@ package com.company.task18;
  */
 public class ExpMain {
     public static void main(String[] args) {
-        try {
-            System.out.println(authentication("login1", "password1", "password1"));
-            System.out.println(authentication("login1login1login1login1login1", "password1", "password1"));
-            System.out.println(authentication("login1", "password1password1password1password1", "password1password1password1password1"));
-            System.out.println(authentication("login1", "password1", "password2"));
-            System.out.println(authentication("login1login1login1login1login1login1", "password1password1password1password1password1", "password2"));
-        } catch (WrongLoginException e) {
-            e.printStackTrace();
-        } catch (WrongPasswordException e) {
-            e.printStackTrace();
-        }
+        System.out.println(authentication("login1", "password1", "password1"));
+        System.out.println(authentication("login1login1login1login1login1", "password1", "password1"));
+        System.out.println(authentication("login1", "password1password1password1password1", "password1password1password1password1"));
+        System.out.println(authentication("login1", "password1", "password2"));
+        System.out.println(authentication("login1login1login1login1login1login1", "password1password1password1password1password1", "password2"));
 
     }
 
-    public static boolean authentication(String login, String password, String confPassword) throws WrongLoginException, WrongPasswordException {
-        if (login.length() > 20) {
-            throw new WrongLoginException("Неправильный логин");
-        }
-        if (password.length() > 20 || !password.equals(confPassword)) {
-            throw new WrongPasswordException("Неправильный пароль");
+    public static boolean authentication(String login, String password, String confPassword) {
+        try {
+            if (login.length() > 20) {
+                throw new WrongLoginException("_______Неправильный логин_______");
+            }
+            if (password.length() > 20 || !password.equals(confPassword)) {
+                throw new WrongPasswordException("_______Неправильный пароль_______");
+            }
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } finally {
+            System.out.println("login - " + login);
+            System.out.println("password - " + password);
+            System.out.println("confPassword - " + confPassword);
         }
         return true;
     }
